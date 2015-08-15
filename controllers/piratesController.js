501a063b078c70fc1bd689dc2652f7ee9b56085c
Maria Nguyen <mnguyen8@mail.pima.edu>
2015-08-15 00:31:36 -0700

    added controllers/

---
+++ controllers/piratesController.js
@@ -0,0 +1,18 @@
+// Controller for Pirates/Users
+
+var Pirate = require('../models/Pirate');
+
+module.exports = {
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ controllers/treasuresController.js
@@ -0,0 +1,7 @@
+// Controllers for Treasures
+
+var Treasure = require('../models/Treasure');
+
+module.exports = {
+
+}
