7ebdfd2b8c59b5f8bf5d8df5a346aa4485c8ee4f
Bryan Garza <bgarza1016@gmail.com>
2015-08-16 15:37:02 -0700

    added about controller

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+//require controllers
+var aboutController = require('../controllers/aboutController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* About page */
+router.get('/about', aboutController.about);
 
 module.exports = router;
