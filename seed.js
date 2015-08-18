5f9dc30fb3a2eb497c06cf8f439b3152f346b56a
Maria Nguyen <mnguyen8@mail.pima.edu>
2015-08-17 17:40:18 -0700

    nodemon fix, in seed file

--- seed.js
+++ seed.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-
+  // mongoose.connect('mongodb://localhost:27017/sidewalk_sailors');
+  console.log('Mongoose connected');
 var Pirate = require('./models/Pirate');
 
 Pirate.remove({}, function(err, entry){
@@ -8,26 +9,30 @@ Pirate.remove({}, function(err, entry){
 });
 
 var newPirates = [
-{
-  username: 'Jong Sparrow',
-  zipcode: 90210,
-  treasure_count: 1
-},
-{
-  username: 'B Beard',
-  zipcode: 92302,
-  treasure_count: 8
-},
-{
-  username: 'Ann Bonnie',
-  zipcode: 90001,
-  treasure_count: 9
-},
-{
-  username: 'Swolliam Wallace',
-  zipcode: 98019,
-  treasure_count: 2
-}
+  {
+    username: 'Jeong@Sparrow.com',
+    password: "123",
+    zipcode: "90210",
+    treasure_count: 1
+  },
+  {
+    username: 'B@Beard.com',
+    password: "123",
+    zipcode: "92302",
+    treasure_count: 8
+  },
+  {
+    username: 'Ann@Bonnie.com',
+    password: "123",
+    zipcode: "90001",
+    treasure_count: 9
+  },
+  {
+    username: 'Swolliam@Wallace.com',
+    password: "123",
+    zipcode: "98019",
+    treasure_count: 2
+  }
 ];
 
 Pirate.create(newPirates, function(err, pirates){
