604480a81a8a590c4cf2d57b37cd9232fad75e8f
Jaytee Sanchez <jaytee.sanchez@gmail.com>
2015-08-19 21:21:40 -0700

    its working now all it needs is styling

--- public/javascripts/google-maps.js
+++ public/javascripts/google-maps.js
@@ -1,10 +1,31 @@
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 34.0500, lng: -118.2500},
-    zoom: 10
-  });
 
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: {lat: 34.0500, lng: -118.2500}
+  });
+  var geocoder = new google.maps.Geocoder();
+    findCurrentLocation(map);
+    geocodeAddress(geocoder, map);
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('zipCode').innerHTML.trim();
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      resultsMap.setCenter(results[0].geometry.location);
+      var infoWindow = new google.maps.InfoWindow({
+        map: resultsMap,
+        position: results[0].geometry.location,
+        content:('Your Treasure!')
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+function findCurrentLocation(map){
 var infoWindow = new google.maps.InfoWindow({map: map});
                 // Try HTML5 geolocation.
                   if (navigator.geolocation) {
@@ -24,7 +45,7 @@ var infoWindow = new google.maps.InfoWindow({map: map});
                     // Browser doesn't support Geolocation
                     handleLocationError(false, infoWindow, map.getCenter());
                   }
-                };
+
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
@@ -32,6 +53,4 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
                         'Error: The Geolocation service failed.' :
                         'Error: Your browser doesn\'t support geolocation.');
 };
-
-
-
+}
